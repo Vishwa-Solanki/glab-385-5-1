3ba5c1d01aac088ac59444843c61a0b9e408f8db
Vishwa-Solanki <vishwasolanki1@gmail.com>
2026-07-17 15:55:13 -0400

    This is final commit

--- v-tank-glab-385-5-1.py
+++ v-tank-glab-385-5-1.py
@@ -2,7 +2,7 @@ import pytz
 from datetime import datetime, time
 
 # Define a function to convert a datetime object between two time zones
-def convert_time_zone(dt, from_tz, to_tz):
+def convert_time_zone(dt, from_tz:String , to_tz: String):
     """
     What this Function do?:
     Converts a datetime object from one time zone to another.
@@ -20,11 +20,13 @@ def convert_time_zone(dt, from_tz, to_tz):
     to_tz_obj = pytz.timezone(to_tz)
 
     # Convert the datetime object to the 'from_tz' timezone (if necessary)
+    print(dt.tzinfo)
+    print(type(from_tz_obj))
     localized_dt_objt = dt.replace(tzinfo=from_tz_obj)
-
+    print(localized_dt_objt.tzinfo)
 
     # Convert the datetime object to the 'to_tz' timezone
-    converted_dt_obj = dt.astimezone(to_tz_obj)
+    converted_dt_obj = localized_dt_objt.astimezone(to_tz_obj)
 
     return converted_dt_obj
 
@@ -37,4 +39,5 @@ current_date = datetime.now()
 # local_timezone2 = pytz.timezone('Pacific/Honolulu')
 
 print(convert_time_zone(current_date,'America/New_York','Pacific/Honolulu'))
+
 
